c3fed25491d32a96fe5bb817df2f0705ce4fded0
Alejandra Escobar <ale.fesico@gmail.com>
2022-04-25 22:28:32 -0500

    Handle POST request method at beer controller

--- src/main/java/com/hcl/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/hcl/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.hcl.msscbrewery.web.controller;
 
 import com.hcl.msscbrewery.services.BeerService;
 import com.hcl.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,6 +21,16 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+        BeerDto beer = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer" + beer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
 
